db58df8c0fa31c71457bfc243efc43ab52285f73
ofthestorm <870771017@qq.com>
2017-09-26 20:46:10 +0800

    marked.js

--- js/script.js
+++ js/script.js
@@ -3,16 +3,39 @@
     $('.left').css('height', windowHeight - 70);
     $('.right').css('height', windowHeight - 70);
 
+    //todo: auto scroll
+    // var displayScrollBar = $('#display-area');
+    // $('.markdown-area').keyup(function () {
+    //     // alert("ha");
+    //     // alert(displayScrollBar.scrollTop);
+    //     displayScrollBar.scrollTop = displayScrollBar.scrollHeight;
+    //     // displayScrollBar.scrollTo('100%');
+    //
+    // });
+    // $('#display-area').stop().animate({
+    //     scrollTop: $('#display-area')[0].scrollHeight
+    // }, 800);
+
 });
 
 var vm = new Vue({
     el: '#markdown',
     data: {
-        message: ""
+        input: "# Hello \n- Try"
     },
+    // computed: {
+    //     haha: function () {
+    //         return this.message+"!";
+    //     }
+    // }
     computed: {
-        displayMessage: function () {
-            return this.message.join('!');
+        compiledMarkdown: function () {
+            return marked(this.input, { sanitize: true })
+        }
+    },
+    methods: {
+        update: function(e) {
+            this.input = e.target.value;
         }
     }
 });
