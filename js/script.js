30e96025648bcde3250750de69415a1d5761be7e
ofthestorm <870771017@qq.com>
2017-09-27 11:05:25 +0800

    ~

--- js/script.js
+++ js/script.js
@@ -4,18 +4,23 @@
     $('.right').css('height', windowHeight - 70);
 
     //todo: auto scroll
-    // var displayScrollBar = $('#display-area');
-    // $('.markdown-area').keyup(function () {
-    //     // alert("ha");
-    //     // alert(displayScrollBar.scrollTop);
-    //     displayScrollBar.scrollTop = displayScrollBar.scrollHeight;
-    //     // displayScrollBar.scrollTo('100%');
-    //
-    // });
+    var displayScrollBar = $('#display-area');
+    $('.markdown-area').keyup(function () {
+        // alert("ha");
+        // alert(displayScrollBar.scrollTop);
+        displayScrollBar.scrollTop = displayScrollBar.scrollHeight;
+        // displayScrollBar.scrollTo('100%');
+
+    });
     // $('#display-area').stop().animate({
     //     scrollTop: $('#display-area')[0].scrollHeight
     // }, 800);
-
+    $('#saveBtn').click(function () {
+        // var win=window.open('','','top=10000,left=10000');
+        // win.document.write(document.getElementById(content).innerHTML);
+        // win.document.execCommand('SaveAs','',filename)
+        // win.close();
+    });
 });
 
 var vm = new Vue({
@@ -56,7 +61,6 @@ function saveFile() {
     win.document.write(document.getElementById(content).innerHTML);
     win.document.execCommand('SaveAs','',filename)
     win.close();
-
 }
 
 //todo: rewrite compile markdown
